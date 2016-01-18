37c52f9801e2611e03fd0d91867c631fb3c37e56
v2kovac <vk.kovac@gmail.com>
2016-01-18 18:34:42 -0500

    fix imports

--- src/main/java/ca/uwaterloo/cs/bigdata2016w/v2kovac/assignment1/PairsPMI.java
+++ src/main/java/ca/uwaterloo/cs/bigdata2016w/v2kovac/assignment1/PairsPMI.java
@@ -14,6 +14,7 @@ import java.util.HashSet;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.io.IntWritable;
