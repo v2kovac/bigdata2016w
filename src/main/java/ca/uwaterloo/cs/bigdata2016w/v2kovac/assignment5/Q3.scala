8351a9b13b9576c65650794439fb47ec9a701eb5
v2kovac <vk.kovac@gmail.com>
2016-02-28 14:43:46 -0500

    q3 start

---
+++ src/main/java/ca/uwaterloo/cs/bigdata2016w/v2kovac/assignment5/Q3.scala
@@ -0,0 +1,54 @@
+package ca.uwaterloo.cs.bigdata2016w.v2kovac.assignment5;
+
+import collection.mutable.HashMap
+import scala.collection.JavaConverters._
+import java.util.StringTokenizer
+import org.apache.log4j._
+import org.apache.hadoop.fs._
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import org.rogach.scallop._
+import org.apache.spark.util.{CollectionsUtils, Utils}
+
+class Conf3(args: Seq[String]) extends ScallopConf(args) {
+  mainOptions = Seq(input, date)
+  val input = opt[String](descr = "input path", required = true)
+  val date = opt[String](descr = "date", required = true)
+}
+
+object Q3 {
+  val log = Logger.getLogger(getClass().getName())
+
+  def main(argv: Array[String]) {
+    val args = new Conf3(argv)
+
+    log.info("Input: " + args.input())
+    log.info("Date: " + args.date())
+
+    val conf = new SparkConf().setAppName("Q1")
+    val sc = new SparkContext(conf)
+
+    val date = args.date()
+    val partMap:Map[Int,String] = Map()
+    val suppMap:Map[Int,String] = Map()
+
+    val part = sc.textFile(args.input() + "/part.tbl")
+    part
+      .foreach(line => {
+        val a = line.split("\\|")
+        partMap + (a(0) -> a(1))
+      })
+
+    val supplier = sc.textFile(args.input() + "/supplier.tbl")
+    supplier
+      .foreach(line => {
+        val a = line.split("\\|")
+        suppMap + (a(0) -> a(1))
+      })
+  }
+}
+
+
+
+
+
